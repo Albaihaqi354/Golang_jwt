5c5422af3c64e85d5f1ccceac37af694a69d88e4
Muhammad_Albaihaqi_Mustofa <bianalbaihaqi190@gmail.com>
2024-08-08 22:35:08 +0700

    [UPDATE] Golang-JWT

--- controllers/user_conteroles.go
+++ controllers/user_conteroles.go
@@ -2,11 +2,17 @@ package controllers
 
 import (
 	"golang_jwt/helpers"
+	"golang_jwt/models"
 	"net/http"
 )
 
 func Me(w http.ResponseWriter, r *http.Request) {
 	user := r.Context().Value("userinfo").(*helpers.MyCustomClaims)
+	userResponse := &models.MyProfile{
+		ID:    user.ID,
+		Email: user.Email,
+		Name:  user.Name,
+	}
 
-	helpers.Response(w, 200, "My Profile", user)
+	helpers.Response(w, 200, "My Profile", userResponse)
 }
